7c940424d41b5faea0d370cdaee64f25137821d0
MelihKocyigitPXL <melih.kocyigit@eeveemobility.com>
2026-05-05 16:14:49 +0200

    Add Berdan testimonial

--- main.js
+++ main.js
@@ -845,9 +845,10 @@ scene("testimonials", (info) => {
     ]);
 
     const npcs = [
-        { name: "Cas", pos: vec2(width() * 0.25, height() / 2), color: rgb(100, 200, 255), testimonial: "Cas: 'Ik heb hem leren kennen als iemand die betrouwbaar is en zijn werk met de nodige zorg en toewijding uitvoert. Hij werkt gestructureerd, denkt mee en is een aangename persoon om mee samen te werken.'" },
-        { name: "Safri", pos: vec2(width() * 0.5, height() / 2), color: rgb(255, 150, 100), testimonial: "Safri: 'Ik waardeer aan jou dat je veel mee bent met wat er in de wereld gebeurt en daar ook boeiend over kan vertellen. Je brengt interessante inzichten en nieuwe ideeën in de groep, en tegelijk maak je de sfeer vaak wat luchtiger.'" },
-        { name: "Ferre", pos: vec2(width() * 0.75, height() / 2), color: rgb(150, 255, 100), testimonial: "Ferre: 'Hij is iemand die altijd 100% geeft en door die inzet weet je dat er altijd een resultaat gaat zijn waar je trots op kan zijn'" },
+        { name: "Cas", pos: vec2(width() * 0.2, height() / 2), color: rgb(100, 200, 255), testimonial: "Cas: 'Ik heb hem leren kennen als iemand die betrouwbaar is en zijn werk met de nodige zorg en toewijding uitvoert. Hij werkt gestructureerd, denkt mee en is een aangename persoon om mee samen te werken.'" },
+        { name: "Safri", pos: vec2(width() * 0.4, height() / 2), color: rgb(255, 150, 100), testimonial: "Safri: 'Ik waardeer aan jou dat je veel mee bent met wat er in de wereld gebeurt en daar ook boeiend over kan vertellen. Je brengt interessante inzichten en nieuwe ideeën in de groep, en tegelijk maak je de sfeer vaak wat luchtiger.'" },
+        { name: "Ferre", pos: vec2(width() * 0.6, height() / 2), color: rgb(150, 255, 100), testimonial: "Ferre: 'Hij is iemand die altijd 100% geeft en door die inzet weet je dat er altijd een resultaat gaat zijn waar je trots op kan zijn'" },
+        { name: "Berdan", pos: vec2(width() * 0.8, height() / 2), color: rgb(255, 100, 255), testimonial: "Berdan: 'Ik heb hem leren kennen tijdens het IT-project en het klikte meteen. Hij is een sterk teamlid om mee te werken en bovendien iemand op wie je binnen een team altijd kunt vertrouwen.'" },
     ];
 
     npcs.forEach((n) => {
